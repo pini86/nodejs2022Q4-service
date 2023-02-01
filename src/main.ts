8fc289e7445fbc2f8c0b390ce8d5297ee98a84eb
Mikhail Druzhinin <pini86@mail.ru>
2023-02-01 18:26:56 +0300

    feat: add swagger api, env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
 import { AppModule } from './app.module';
+import 'dotenv/config';
+import { parse } from 'yaml';
+
+const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const apiSchema = await readFile(
+    join(__dirname, '..', 'doc/api.yaml'),
+    'utf-8',
+  );
+
+  SwaggerModule.setup('api', app, parse(apiSchema));
+
+  await app.listen(PORT);
 }
 bootstrap();
