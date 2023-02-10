326619d24bd6c5adca8535e23418beba3ac62548
Mikhail Druzhinin <pini86@mail.ru>
2023-02-10 12:20:36 +0300

    feat: add typeorm configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { TrackModule } from './track/track.module';
 import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
 import { FavoritesModule } from './favorite/favorites.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from './ormconfig';
 
 @Module({
   imports: [
@@ -14,6 +16,7 @@ import { FavoritesModule } from './favorite/favorites.module';
     ArtistModule,
     AlbumModule,
     FavoritesModule,
+    TypeOrmModule.forRoot(config),
   ],
   controllers: [AppController],
   providers: [AppService],
