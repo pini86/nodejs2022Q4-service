857f9455e427afe8480b86e6d09290934972b519
Mikhail Druzhinin <pini86@mail.ru>
2023-02-15 11:16:42 +0300

    refactor: users migration

--- migrations/1676275641189-CreateUsers.ts
+++ migrations/1676275641189-CreateUsers.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class CreateUsers1658431259555 implements MigrationInterface {
-  name = 'CreateUsers1658431259555';
+export class CreateUsers1676275641189 implements MigrationInterface {
+  name = 'CreateUsers1676275641189';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
